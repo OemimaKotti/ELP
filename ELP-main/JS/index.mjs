25e9f53f70d3929e601f60840cd190e95b2e9c86
OemimaKotti <omeima.kotti@insa-lyon.fr>
2025-01-09 13:16:32 +0100

    Delete ELP-main directory

--- ELP-main/JS/index.mjs
+++
@@ -1,2 +0,0 @@
-import { runGame } from './game.mjs';
-runGame();
--- ELP-main/JS/mat.mjs
+++
@@ -1,9 +0,0 @@
-export const initializeMats = async () => {
-    const player1Mat = Array(7).fill('');
-    const player2Mat = Array(7).fill('');
-    return { player1Mat, player2Mat };
-};
-
-export const isValidNoun = async (word) => {
-    return word.length >= 3;
-};
